f70b29faabe14fc8bff90c591e60233e929605fc
Wahaj <wahajahmad19@gmail.com>
2024-09-16 02:54:43 +0500

    Node Express and All Dependencies Install

---
+++ server.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import config from 'config'
+import debugg from 'debug'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import mongoDb from 'mongoose'
+// import banks from './src/routers/banks'
+// import customers from './src/routers/customers'
+
+const server = express()
+const port = process.env.PORT ?? 2123;
+const debug = debugg(config.get('debug'))
+const debugDb = debugg(config.get('debug-db'))
+const ENVIRONMENT = 'env'
+
+server.use(express.json(), express.static('public'), cors(), helmet(), express.urlencoded({extended: true}))
+
+// mongoDb.connect('mongodb://0.0.0.0:27017/tnsa')
+//     .then((): void => debugDb('Database: Connected to MongoDB'))
+//     .catch(err => debugDb(`Database: Connection failed due to: ${err}`))
+
+if(server.get(ENVIRONMENT) === 'development') { 
+    server.use(morgan('dev'))
+    debug('Environment: Development')
+}
+else if(server.get(ENVIRONMENT) === 'production') {
+    debug('Environment: Production')
+}
+
+// server.use('/', home)
+// server.use('/promise', promise)
+// server.use('/reminders', reminders)
+// server.use('/banks', banks)
+// server.use('/customers', customers)
+
+server.listen(port, (): void => debug(`Server started on port ${port}`))
