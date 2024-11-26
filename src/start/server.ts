71af12f353e693dff1f51de33b05c3a57d0968e6
WahaaJAli <73385761+WahaaJAli@users.noreply.github.com>
2024-11-26 23:04:57 +0500

    Server Startup Fix

--- src/start/server.ts
+++ src/start/server.ts
@@ -3,38 +3,38 @@ import app from './app'
 import connectToDatabase from './mongoose'
 import logger from '../utils/logger'
 import mongoose from 'mongoose'
- 
+
 const startServer = async (): Promise<void> => {
   try {
     await connectToDatabase()
     const server = app.listen(config.PORT, () => logger.info(`Server started on port ${config.PORT}`))
 
     const gracefulShutdown = async () => {
-        try {
-          logger.info('Server shutting down...')
-          await Promise.all([
-            mongoose.connection.close().then(() => logger.info('Database connection closed.')),
-            new Promise<void>((resolve, reject) => {
-              server.close((err) => {
-                if (err) return reject(err)
-                logger.info('HTTP server closed.')
-                resolve()
-              })
+      try {
+        logger.info('Server shutting down...')
+        await Promise.all([
+          mongoose.connection.close().then(() => logger.info('Database connection closed.')),
+          new Promise<void>((resolve, reject) => {
+            server.close((err) => {
+              if (err) return reject(err)
+              logger.info('HTTP server closed.')
+              resolve()
             })
-          ])
-          process.exit(0)
-        }
-        catch (error) {
-          logger.error(`Error during graceful shutdown: ${(error as Error).message}`)
-          process.exit(1)
-        }
-    }      
+          })
+        ])
+        process.exit(0)
+      }
+      catch (error) {
+        logger.error(`Error during graceful shutdown: ${(error as Error).message}`)
+        process.exit(1)
+      }
+    }
 
     process.on('SIGINT', gracefulShutdown)
     process.on('SIGTERM', gracefulShutdown)
   }
   catch (error) {
-    logger.error(`Server failed to start: ${(error as {message: string}).message}`)
+    logger.error(`Server failed to start: ${(error as { message: string }).message}`)
     process.exit(1)
   }
 }
@@ -42,7 +42,7 @@ const startServer = async (): Promise<void> => {
 startServer()
 
 process.on('uncaughtException', (error: Error) => {
-  logger.error(`Uncaught Exception: ${error.message}`, {stack: error.stack})
+  logger.error(`Uncaught Exception: ${error.message}`, { stack: error.stack })
   process.exit(1)
 })
 
