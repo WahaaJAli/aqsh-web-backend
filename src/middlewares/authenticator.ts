6689a15d8f827276417a368ac9dc921ad41e763d
WahaaJAli <73385761+WahaaJAli@users.noreply.github.com>
2024-11-27 12:03:44 +0500

    MongoDB Install

--- src/middlewares/authenticator.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express"
-
-const authenticator = (req: Request, res: Response, next: NextFunction) => {
-    console.log('...authenticating')
-    next()
-}
-
-export default authenticator
