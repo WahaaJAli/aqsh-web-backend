48242119a210f6008c18c07558fa8e95b48470b3
WahaaJAli <73385761+WahaaJAli@users.noreply.github.com>
2024-12-16 14:48:03 +0500

    Mongoose Logger Update

--- src/start/mongoose.ts
+++ src/start/mongoose.ts
@@ -1,13 +1,13 @@
 import config from '../config/Environment'
 import logger from '../utils/logger'
-import mongoose from 'mongoose'
+import mongoose, { Mongoose } from 'mongoose'
 
 mongoose.connection.on('disconnected', () => logger.info('MongoDB connection closed.') )
 
 export default async (): Promise<void> => {
   try {
-    await mongoose.connect(config.DATABASE!)
-    logger.info('Database: Connected to MongoDB')
+    const { connection: { host } }: Mongoose = await mongoose.connect(config.DATABASE!)
+    logger.info(`MongoDB Connected: ${host}`)
   }
   catch (error) {
     logger.error(`Database connection error: ${(error as Error).message}`)
